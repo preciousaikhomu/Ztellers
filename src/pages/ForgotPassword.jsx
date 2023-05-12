d32cb6ff4f5d402e71d299060e34fc0dac1ffa59
Phuc Nguyen T <phuc.nguyent@s3corp.com.vn>
2023-05-12 14:38:27 +0700

    complete the forgot password page functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -3,14 +3,29 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
 
-export default function ForgotPassword() {
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
+export default function ForgotPassword() {
+  //We can use a method called send password reset email from Firebase for Slash off, which is very similar to the one we did for the sign up and signing page.
+  //
 
   const [email, setEmail] = useState("");
 
   const onchange = (e) => {
     setEmail(e.target.value);
   };
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
+  }
 
   return (
     <section>
@@ -24,7 +39,7 @@ export default function ForgotPassword() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
               type="email"
@@ -33,7 +48,7 @@ export default function ForgotPassword() {
               onChange={onchange}
               placeholder="Email address"
             />
-            
+
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="mb-6">
                 Don't have a account?
@@ -57,7 +72,7 @@ export default function ForgotPassword() {
               type="submit"
               className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
             >
-             Send reset password
+              Send reset password
             </button>
             <div className="flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
               <p className="text-center font-semibold mx-4">OR</p>
