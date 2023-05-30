c174735d7218515d6b80072b09127f158dfef3b7
ntphuc0987 <truelove309@gmail.com>
2023-05-30 09:56:46 +0700

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -17,10 +17,13 @@ import {
 } from "swiper";
 import "swiper/swiper-bundle.css";
 
+import { FaShare } from "react-icons/fa";
+
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   useEffect(() => {
     //we need to have a function to get the data.
@@ -35,11 +38,13 @@ export default function Listing() {
       if (docSnap.exists()) {
         setListing(docSnap.data());
         setLoading(false);
-        console.log(listing);
+        //console.log(listing);
       }
     };
     fetchListing();
+    //console.log(listing);
   }, [params.listingId]);
+
   if (loading) {
     return <Spinner />;
   }
@@ -66,6 +71,23 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibol border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
