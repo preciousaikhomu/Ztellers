dd70ded739221fba57683c24315d138b94e63ba3
ntphuc0987 <truelove309@gmail.com>
2023-05-31 11:15:47 +0700

    Add the listing information to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { db } from "../firebase";
 import { doc, getDoc } from "firebase/firestore";
 import Spinner from "../components/Spinner";
 
+
 //import { Swiper, SwiperSlide } from "swiper/react";
 //import SwiperCore, {  EffectFade,  Autoplay,  Navigation,  Pagination,} from "swiper";
 
@@ -17,7 +18,8 @@ import {
 } from "swiper";
 import "swiper/swiper-bundle.css";
 
-import { FaShare } from "react-icons/fa";
+import { FaShare, FaMapMarkedAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa";
+
 
 export default function Listing() {
   const params = useParams();
@@ -83,11 +85,65 @@ export default function Listing() {
       >
         <FaShare className="text-lg text-slate-500" />
       </div>
-      {shareLinkCopied && (
-        <p className="fixed top-[23%] right-[5%] font-semibol border-2 border-gray-400 rounded-md bg-white z-10 p-2">
-          Link Copied
-        </p>
-      )}
+      {
+        //if the shearling is true. show this here that we want to have a paragraph.
+        shareLinkCopied && (
+          <p className="fixed top-[23%] right-[5%] font-semibol border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+            Link Copied
+          </p>
+        )
+      }
+
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+        <div className="w-full  lg-[400px]">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.name} - ${" "}
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" ? " / month" : ""}
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold ">
+            <FaMapMarkedAlt className="text-green-700 mr-1" /> {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+              <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md ">
+                {listing.type === "rent" ? "Rent" : "Sale"}
+              </p>
+              {listing.offer && (
+                <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">
+                  ${(+listing.regularPrice) - (+listing.discountedPrice)} discount
+                </p>
+              )}
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold">Description - </span>{listing.description}
+          </p>
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+            <li className="flex items-center whitespace-nowrap">
+                <FaBed className="text-lg mr-1" />
+              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+                <FaBath className="text-lg mr-1" />
+              {+listing.bathrooms > 1 ? `${listing.bathrooms} baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+                <FaParking className="text-lg mr-1" />
+              {+listing.parking ? "Parking Spot" : "No Parking"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+                <FaChair className="text-lg mr-1" />
+              {+listing.furnished ? "Furnished" : "Not Furnished"}
+            </li>
+          </ul>
+        </div>
+        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+      </div>
     </main>
   );
 }
