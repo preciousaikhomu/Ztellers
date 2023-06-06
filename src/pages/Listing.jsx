42205a583b734481e69716a7592326318db8a415
ntphuc0987 <truelove309@gmail.com>
2023-06-06 14:05:07 +0700

    Add Map to the listing page using leaflet and react -leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -29,6 +29,8 @@ import {
 import { getAuth } from "firebase/auth";
 import Contact from "../components/Contact";
 
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 export default function Listing() {
   const auth = getAuth();
   const params = useParams();
@@ -171,10 +173,29 @@ export default function Listing() {
           }
         </div>
         {contactLandLord && (
-            <Contact userRef={listing.userRef} listing={listing} />
-          )}
-       
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+          <Contact userRef={listing.userRef} listing={listing} />
+        )}
+
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                {listing.address}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   );
