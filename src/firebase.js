ba097df67459a15772fdc080271a31fe7126f65a
Dev Royale <unityekeoba18@gmail.com>
2024-04-02 14:02:52 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  AIzaSyBSxBUU2VcpQYjqK8rP19v0FDgCEPXsBl8",
+ apiKey: "AIzaSyBSxBUU2VcpQYjqK8rP19v0FDgCEPXsBl8",
   authDomain: "playground-14bcf.firebaseapp.com",
   databaseURL: "https://playground-14bcf-default-rtdb.firebaseio.com",
   projectId: "playground-14bcf",
   storageBucket: "playground-14bcf.appspot.com",
   messagingSenderId: "601759179504",
-  appId: "1:601759179504:web:1ebe3d07b043a531445835"
+  appId: "1:601759179504:web:1ebe3d07b043a531445835",
 };
 
 // Initialize Firebase
