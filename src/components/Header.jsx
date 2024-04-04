f727a68fbcc047b2a39b36dd63252fd6827cdb6a
Dev Royale <unityekeoba18@gmail.com>
2024-04-04 11:32:52 +0100

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLocation , useNavigate} from "react-router-dom"
 import {getAuth, onAuthStateChanged} from "firebase/auth";
+import logo from "../assets/svg/logo.svg"
 
 export default function Header () {
     
@@ -38,7 +39,7 @@ export default function Header () {
         <div className="bg-white border-b shadow-sm sticky top-0 z-40">
             <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
                 <div>
-                    <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className="h-5 cursor-pointer" onClick={()=>navigate("/")}/>
+                    <img src={logo} alt="logo" className="h-5 cursor-pointer" onClick={()=>navigate("/")}/>
                 </div>
                 <div>
                     <ul className="flex space-x-10">
